6d355900c2297a1de77f254bb84caee51bdb4b3b
Harshad Sharma <harshad@harshadsharma.com>
2012-08-19 00:30:56 +0530

    Helper functions to store and search for text by keywords.

--- djapp/database/models.py
+++ djapp/database/models.py
@@ -8,6 +8,40 @@ class Message(models.Model):
     user = models.ForeignKey(User)
     keyword = models.ForeignKey('Keyword', blank=True, null=True)
 
+    @staticmethod
+    def store(email, text, keyword=None):
+        """Stores given text and assigns it to user, creates Django user if
+        matching user is not found."""
+        try:
+            user = User.objects.get(email=email)
+        except:
+            username = email.replace('.', '')
+            username = username.replace('@', '')
+            user = User(email=email, username=username).save()
+            print "User", user, "created."
+
+        Message(text=text, user=user, keyword=keyword).save()
+
+    @staticmethod
+    def get_by_search(email, words_to_match):
+        """Gets messages with text that matches all words in the list."""
+        try:
+            user = User.objects.get(email=email)
+        except:
+            #TODO Raise a sane error
+            raise
+
+        return Message.get_recursive_union_filter(Message.objects, words_to_match, user)
+
+    @staticmethod
+    def get_recursive_union_filter(message_objects, words, user):
+        if len(words) == 0:
+            return message_objects
+
+        words_new = words[1:]
+        return Message.get_recursive_union_filter(message_objects.filter(text__contains=words[0], user=user), words_new, user)
+
+
 
 class Keyword(models.Model):
     """Stores keywords for easy indexing."""
